e046c0cbe8c24cd0a7217c4def56a8f6051b2d03
Mason Glidden <mason@masonglidden.com>
2015-11-09 15:41:29 -0500

    Bump package.js version

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 Package.describe({
     summary: "Helpers for writing unit tests with Munit",
-    version: "0.3.1",
+    version: "0.3.2",
     name: "tulip:munit-helpers",
     git: "https://github.com/tulip/munit-helpers.git"
 });
