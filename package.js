25ac647f463b7a3fcdb2b9725e163a3f2b5c300a
Ben Weissmann <bsw@tulip.co>
2016-02-09 12:24:56 -0500

    Bump version

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ limitations under the License.
 
 Package.describe({
     summary: "Helpers for writing unit tests with Munit",
-    version: "0.3.3",
+    version: "0.3.4",
     name: "tulip:munit-helpers",
     git: "https://github.com/tulip/munit-helpers.git"
 });
